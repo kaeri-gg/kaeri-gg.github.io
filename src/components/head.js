47baf70b8ae7c51fd38004515c8dec3e4e95598b
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2025-02-27 19:00:28 +0800

    updated favicon

--- src/components/head.js
+++ src/components/head.js
@@ -18,7 +18,6 @@ const Head = ({ title, description, image }) => {
             defaultDescription: description
             siteUrl
             defaultImage: image
-            instagramUsername
           }
         }
       }
@@ -48,6 +47,13 @@ const Head = ({ title, description, image }) => {
       <meta property="og:type" content="website" />
 
       <meta name="google-site-verification" content="fXnCu2ds7rsAnsI4doUbWe_GjUFvhEpao24X23UsnIY" />
+
+      <link rel="icon" type="image/png" href="../images/favicons/favicon-96x96.png" sizes="96x96" />
+      <link rel="icon" type="image/svg+xml" href="../images/favicons/favicon.svg" />
+      <link rel="shortcut icon" href="../images/favicons/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="../images/favicons/apple-touch-icon.png" />
+      <meta name="apple-mobile-web-app-title" content="Kaeri_gg" />
+      <link rel="manifest" href="../images/favicons/site.webmanifest" />
     </Helmet>
   );
 };
