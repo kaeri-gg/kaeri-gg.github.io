fe5f4e9703411bf3927cba2e21d57aa67f1a7e6d
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2025-03-01 00:11:49 +0800

    updated portfolio.

--- src/config.js
+++ src/config.js
@@ -41,8 +41,8 @@ module.exports = {
 
   colors: {
     green: '#64ffda',
-    navy: '#0a192f',
-    darkNavy: '#020c1b',
+    navy: '#03040e',
+    darkNavy: '#03040e',
   },
 
   srConfig: (delay = 200, viewFactor = 0.25) => ({
