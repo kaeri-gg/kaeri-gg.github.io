0d1479af08240fac94be774c8a2a3df102abae6a
kaeri-gg <kaeri-gg@users.noreply.github.com>
2025-06-29 16:37:00 +0000

    deploy: 89f056810465ce4a72cc91aeebdccc77b8bf4a0e

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-041c1bb63f1dae74d9a7.js"
+    "url": "webpack-runtime-c11ce879042f12412e72.js"
   },
   {
     "url": "framework-910e9ae1098cefc1fc8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fdc9e09f116dd1c009c8b5c87a96c0b0"
+    "revision": "d8d55c215a554732ede20d573edcee84"
   },
   {
     "url": "manifest.webmanifest",
